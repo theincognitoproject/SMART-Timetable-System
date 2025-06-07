1cdc882f80fc77531ff3d92d39203bfa4ab1fbd5
Jakevernice <jakevernice@github.com>
2025-06-07 22:55:40 +0530

    feat: Configure Vite server settings for deployment with allowed hosts and port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['smart-timetable-system.onrender.com'],
+    host: true, // also helpful when deploying
+    port: 4173,
+  }
 })
